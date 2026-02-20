cd1f9d3c3f9c06cc28213dbe3bf40f0ba9d79d37
Faahim15 <abdulkaderfahim6@gmail.com>
2026-02-20 16:16:36 +0600

    grocery comparison app initiated

---
+++ src/constants/styles.tsx
@@ -0,0 +1,8 @@
+import { scale, verticalScale } from "../utils/Scaling";
+
+export const arrowStyles = {
+  width: scale(44),
+  height: verticalScale(44),
+  borderRadius: scale(22),
+  backgroundColor: "#ffffff",
+};
